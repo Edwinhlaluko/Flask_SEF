fea13e964a2358bb5807b0bd2562b58cd9163b0b
edwinhlaluko@gmail.com <edwinhlalujo@gmail.com>
2023-04-17 16:02:58 +0200

    added changes

--- main.py
+++ main.py
@@ -1,62 +1,87 @@
-from flask import Flask, render_template, request, jsonify
-import tensorflow as tf
+import streamlit as st
 import pandas as pd
-import numpy as np
-from sklearn.preprocessing import MinMaxScaler
+import matplotlib.pyplot as plt
+import seaborn as sns
+from joblib import load
+import joblib as jb
+import plotly.express as px
 
-app = Flask(__name__)
+# Load the machine learning models
+model1 = jb.load('linear_regression_model.joblib')
+df = pd.read_csv('ESK2033.csv')
+df = df.dropna()
+#model2 = load('linear_regression_model.joblib')
 
-# Load the models
-model1 = tf.keras.models.load_model('Capacity_model_24.h5')
-model2 = tf.keras.models.load_model('Demand_model_24.h5')
+# Define a function to make predictions using the models
+def predict(model, input_data):
+    prediction = model.predict(input_data)
+    return prediction
 
-data = pd.read_csv('ESK2033.csv')
+# Define the layout of the app
+st.set_page_config(page_title='Energy Dashboard App')
+st.title('Energy Dashboard App')
 
-# Load the scalers
-scaler1 = MinMaxScaler(feature_range=(0, 1))
-scaler1.fit_transform(data)
-scaler2 = MinMaxScaler(feature_range=(0, 1))
-scaler2.fit_transform(data)
+# Add a sidebar
+st.sidebar.title('Navigation')
+menu = ['Home', 'Model 1', 'Model 2']
+choice = st.sidebar.selectbox('Select a page', menu)
 
-# Define the home page route
-@app.route('/')
-def home():
-    return render_template('index.html')
+# Define the content of the pages
+if choice == 'Home':
+    st.header('Welcome to the Energy Dashboard App!')
+    st.write('This app allows you to predict energy consumption based on different variables.')
+    st.write('Please select a page from the sidebar to continue.')
 
-# Define the route for model 1 predictions
-@app.route('/model1', methods=['POST'])
-def predict_model1():
-    # Get the data from the form
-    data = request.form['data']
+elif choice == 'Model 1':
+    st.header('Model 1')
+    st.write('This model predicts energy consumption based on variables ResidualDemand, RSAContractedDemand, ThermalGeneration, PumpedWaterGeneration, and PumpedWaterSCOPumping.')
+    with st.form(key='model1_form'):
+        rsa_contracted_demand = st.number_input('RSA Contracted Demand', min_value=0, max_value=100, value=50, step=1)
+        thermal_generation = st.number_input('Thermal Generation', min_value=0, max_value=100, value=50, step=1)
+        pumped_water_generation = st.number_input('Pumped Water Generation', min_value=0, max_value=100, value=50, step=1)
+        pumped_water_sco_pumping = st.number_input('Pumped Water SCO Pumping', min_value=0, max_value=100, value=50, step=1)
+        submit_button = st.form_submit_button(label='Predict')
+    if submit_button:
+        input_data = [[rsa_contracted_demand, thermal_generation, pumped_water_generation, pumped_water_sco_pumping]]
+        prediction = predict(model1, input_data)
+        previous_demand = df['Residual Demand'].iloc[-2]
+        percent_change = (prediction[0] - previous_demand) / previous_demand * 100
+        st.success(f'Previous demand: {previous_demand:.2f}\nPredicted demand: {prediction[0]:.2f} ({percent_change:.2f}%)')
 
-    # Preprocess the data
-    data = pd.DataFrame(data)
-    data = scaler1.transform(data)
-    X = np.array(data)
+    fig = px.line(df, x=df.index, y='Residual Demand')
+    
+    st.plotly_chart(fig)
 
-    # Make a prediction
-    prediction = model1.predict(X)
-
-    # Return the result as JSON
-    return jsonify({'result': prediction.tolist()})
-
-# Define the route for model 2 predictions
-@app.route('/model2', methods=['POST'])
-def predict_model2():
-    # Get the data from the form
-    data = request.form['data']
-
-    # Preprocess the data
-    data = pd.DataFrame(data)
-    data = scaler2.transform(data)
-    X = np.array(data)
-
-    # Make a prediction
-    prediction = model2.predict(X)
-
-    # Return the result as JSON
-    return jsonify({'result': prediction.tolist()})
-
-# Run the app
-if __name__ == '__main__':
-    app.run(debug=True)
+elif choice == 'Model 2':
+    st.header('Model 2')
+    st.write('This model predicts energy consumption based on variables Y1, Y2, Y3, and Y4.')
+    with st.form(key='model2_form'):
+        y1 = st.number_input('Y1', min_value=0, max_value=100, value=50, step=1)
+        y2 = st.number_input('Y2', min_value=0, max_value=100, value=50, step=1)
+        y3 = st.number_input('Y3', min_value=0, max_value=100, value=50, step=1)
+        y4 = st.number_input('Y4', min_value=0, max_value=100, value=50, step=1)
+        submit_button = st.form_submit_button(label='Predict')
+    if submit_button:
+        input_data = [[y1, y2, y3, y4]]
+        prediction = predict(model2, input_data)
+        st.success(f'The predicted energy consumption is {prediction[0]:.2f} units.')
+# Add a footer
+st.sidebar.text('')
+st.sidebar.text('')
+st.sidebar.text('')
+st.sidebar.text('')
+st.sidebar.text('')
+st.sidebar.text('')
+st.sidebar.text('')
+st.sidebar.text('')
+st.sidebar.text('')
+st.sidebar.text('')
+st.sidebar.text('')
+st.sidebar.text('')
+st.sidebar.text('')
+st.sidebar.text('')
+st.sidebar.text('')
+st.sidebar.text('')
+st.sidebar.text('')
+st.sidebar.text('')
+st.sidebar.text('Developed by No-Name')
